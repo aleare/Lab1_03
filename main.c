149929508bd151310208a3df04b3d6d4edf875f2
David Costa <S229089@studenti.polito.it>
2017-10-18 16:59:50 +0200

    v1

--- main.c
+++ main.c
@@ -1,17 +1,25 @@
 #include "Librerie/libreria_principale.h"
-#ifdef MAX_VETT
-#undef MAX_VETT
-#define MAX_VETT 100000
-//NON FUNZIONA NELLA LIBRERIA
-#endif
+#define MAX_RIGHE 30
+#define MAX_PAROLA 20
+
 int main() {
-    hello_world();
+    FILE *f1; FILE *f2; int r=0;
+    char parola[MAX_PAROLA]; int x=0;
+    char sost[MAX_PAROLA][MAX_PAROLA]={0,0};
+    printf("LABORATORIO 1 ES 3:\n");
+    //Apertura Files...
+    printf("Primo file:\n");
+    f1=FILE_apri_file(FILENAME_MAX,"r");
+    printf("Secondo file:\n");
+    f2=FILE_apri_file(FILENAME_MAX,"r");
+    //Lettura di sost.
+    fscanf(f2," %d ",&r);
+    for (int i = 0; i < r; ++i) {
+        fscanf(f2," %d %s",&x,parola);
+        strcpy(sost[x],parola);
+        if (feof(f2)) exit(EXIT_FAILURE);
+    }
     return 0;
 }
-/*Domande sono:
-1) Ho fatto bene ?
-2) I Macro come funzionano
-3) Funzioni che accettano parametri diversi
-4) Come fare LOG
-5) Come modificare le costanti anche nella libreria? */
+
 
